5adfbcb5b504444806916fc9461cc175f16a9165
gandalf0151 <gandalfthewhite151@gmail.com>
2020-07-25 00:27:04 +0530

    Jul 16, 2020:Started SPOJ

---
+++ SPOJ/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while(cin>>n)
+    {
+        if(n!=42)
+            cout<<n<<endl;
+        else
+            break;
+    }
+    // your code goes here
+    return 0;
+}
